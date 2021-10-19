aa07b636e30f8af9327454099fe07430177f9b23
Jakov Knezevic <knezevic.jakov@gmail.com>
2021-10-19 17:14:25 +0200

    ErrorBoundry feature added!

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -14,9 +15,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ robots: users }));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -24,29 +25,27 @@ class App extends Component {
   };
 
   render() {
-    const {robots, searchfield} = this.state;
+    const { robots, searchfield } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    return !robots.length ?
-      (
-        <div className="tc">
-          <h1 className="f1">Loading...</h1>
-        </div> 
-      ) :
-      (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
+    return !robots.length ? (
+      <div className="tc">
+        <h1 className="f1">Loading...</h1>
+      </div>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
             <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      ); 
-    }
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
   }
+}
 
 export default App;
